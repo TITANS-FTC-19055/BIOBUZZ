ee16503f8de36d3cf3383cab1a87cd2719f20abc
PepeneleZ <pepegame96@gmail.com>
2026-09-16 18:22:49 +0300

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/tests/TestServo.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.lib.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Disabled
+@TeleOp(name = "TestServo", group = "Test")
+public class TestServo extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+         Servo servo = hardwareMap.get(Servo.class, "servo");
+         servo.setDirection(Servo.Direction.FORWARD);
+
+         waitForStart();
+
+         servo.setPosition(1);
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/tests/TestTouchSensor.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.lib.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+@Disabled
+@TeleOp(name = "TestTouchSensor", group = "Test")
+public class TestTouchSensor extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        TouchSensor sensor = hardwareMap.get(TouchSensor.class, "touch sensor");
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            telemetry.addData("sensor", sensor.getValue());
+            telemetry.update();
+        }
+    }
+}
