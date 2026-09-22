39b0c38ba61bed9a9767ef062db6c9ccaad2cad8
Alex Anculescu <alexanculescu@Alexs-MacBook-Air.local>
2026-09-22 18:05:40 +0300

    program plus incercare de de unghi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Turret.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.hardware.subsystems;
 
 import androidx.annotation.NonNull;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -16,7 +17,7 @@ public class Turret implements Updateable {
 
         public DcMotorEx launch1;
         public DcMotorEx launch2;
-        private final Servo hrot1, hrot2;
+        private final CRServo hrot1, hrot2;
         public Servo block;
 
 
@@ -29,18 +30,22 @@ public class Turret implements Updateable {
         //  public Servo two;
 
         public shooting_states state;
+        public DcMotorEx rotation_encoder;
+        public double ticksPerRotation = 8192;
+        public double rotation_target = 0;
 
         public double voltage, velocity, nominalVoltage = 9.3;
         PIDController controller = new PIDController(kp, 0, 0);
+        PIDController rotationController = new PIDController(0,0,0);
         SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(ks, kv, ka);
 
         public Turret(@NonNull HardwareMap hwmap) {
-
+            rotation_encoder = hwmap.get(DcMotorEx.class, HardwareConfig.rotation_encoder);
             launch1 = hwmap.get(DcMotorEx.class, HardwareConfig.launch1);
             launch2 = hwmap.get(DcMotorEx.class, HardwareConfig.launch2);
             launch2.setDirection(DcMotorSimple.Direction.REVERSE);
-            hrot1 = hwmap.get(Servo.class, HardwareConfig.hrot1);
-            hrot2 = hwmap.get(Servo.class, HardwareConfig.hrot2);
+            hrot1 = hwmap.get(CRServo.class, HardwareConfig.hrot1);
+            hrot2 = hwmap.get(CRServo.class, HardwareConfig.hrot2);
             block = hwmap.get(Servo.class, HardwareConfig.block);
             launch1.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
             launch1.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
@@ -51,18 +56,23 @@ public class Turret implements Updateable {
         @Override
         public void update() {
 
-
+            double rotation = rotation_encoder.getCurrentPosition();
             velocity = launch1.getVelocity();
             controller.setPID(kp, 0, 0);
+            rotationController.setPID(0,0,0);
             feedforward = new SimpleMotorFeedforward(ks, kv, ka);
+            double rotationPIDoutput = rotationController.calculate(rotation, rotation_target);
             double PID_output = controller.calculate(velocity, target);
             double ff_output = feedforward.calculate(target);
             launch1.setPower((PID_output + ff_output) * (nominalVoltage / voltage));
             launch2.setPower((PID_output + ff_output) * (nominalVoltage / voltage));
+            hrot1.setPower(rotationPIDoutput);
+            hrot2.setPower(rotationPIDoutput);
             if (launch1.getVelocity() < 100 && state == shooting_states.OFF) {
                 launch1.setPower(0);
                 launch2.setPower(0);
             }
+
 
 
         }
@@ -88,6 +98,9 @@ public class Turret implements Updateable {
             } else state = statex;
             runPid = true;
             target = state.val;
+        }
+        public void setHorizontalAngle(double angleInDegrees){
+            rotation_target = (8192*angleInDegrees)/360;
         }
     /*public void setAngle(double angle){
         one.setPosition(angle);two.setPosition(angle);
